4439b9d36a7fbd0646b56db1792d53a6d874909b
leonardo souza de andrade <leonardo.andrade@aluno.fmpsc.edu.br>
2024-11-29 08:50:45 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const links = document.querySelectorAll('a');
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        alert('Você será redirecionado para ' + link.href);
+    });
+});
